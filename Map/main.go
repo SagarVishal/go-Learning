38c738166657d1991f89bb99bed4e8c389cb7d92
Vishal <vishal@improwised.com>
2021-05-26 19:31:07 +0530

    Map # Playing around with map

--- Map/main.go
+++ Map/main.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	// var colors map[string]string
 
-	colors := make(map[int]string)
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
-	// colors := map[string]string{
-	// 	"red" : "#ff0000",
-	// 	"green" : "#4bf745",
-	// }
+	printMap(colors)
+}
 
-	colors[10] = "#ffffff"
-
-	delete(colors, 10)
-
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
