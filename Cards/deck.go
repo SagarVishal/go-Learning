298f10300d6beccfd58862ba405e0b057cabbd27
Vishal <vishal@improwised.com>
2021-05-26 12:47:34 +0530

    Default empty value of struct and reassignment

--- Cards/deck.go
+++ Cards/deck.go
@@ -34,7 +34,10 @@ func (d deck) print() {
 	}
 }
 
-func deal(d deck, handSizfilename
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
 func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
 
